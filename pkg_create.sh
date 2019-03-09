f78736a480196f5b4f9af59a8287b717b1998b09
Dmitry Marov <d.marov94@gmail.com>
2019-03-09 17:05:06 +0300

    frebsd pkg build added

--- pkg_create.sh
+++ pkg_create.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-pkg create -m $PWD/metadata
+cp $PWD/target/release/i3blocks_date $PWD/rootdir/usr/localbin/i3blocks_date
+pkg create -m $PWD/metadata -o $PWD/pkg-builds -p $PWD/metadata/PLIST -r $PWD/rootdir
